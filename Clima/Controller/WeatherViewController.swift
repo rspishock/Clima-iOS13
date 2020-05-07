76ee7a7420e06182074544075e2ab1b79d36d139
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-05-07 15:37:55 -0400

    Project complete.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,18 +7,106 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()  // Part of CoreLocation
+    
+    //    let units = ["metric", "imperial", "kelvin"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Delegate must be set before being called.
+        locationManager.delegate = self
+        // Requests user permission to access location
+        locationManager.requestWhenInUseAuthorization()
+        // Requests location once (non-repeating).
+        locationManager.requestLocation()
+        
+        weatherManager.delegate = self
+        searchTextField.delegate = self
     }
-
-
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+    }
+    
 }
 
+
+//MARK: - UITextFieldDelegate
+
+extension WeatherViewController: UITextFieldDelegate {
+    @IBAction func searchPressed(_ sender: UIButton) {
+            searchTextField.endEditing(true)
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchTextField.endEditing(true)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if textField.text != "" {
+                return true
+            } else {
+                textField.placeholder = "Enter a city"
+                return false
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            if let city = searchTextField.text {
+                weatherManager.fetchWeather(cityName: city)
+    //            weatherManager.fetchWeather(cityName: city, unit: unit)
+            }
+            searchTextField.text = ""
+        }
+}
+
+
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        // Calls main thread to update UI in the background
+        DispatchQueue.main.sync {
+            self.cityLabel.text = weather.cityName
+            self.temperatureLabel.text = weather.temperatureString
+            conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+        
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+
+//MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last { // grabs last found location
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
